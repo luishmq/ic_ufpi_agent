edbe11b151e2b1bfaafc8698de229c423206cde2
“Luis” <mrqueiroz22014@gmail.com>
2025-03-08 14:01:34 -0300

    feat: Add nova feature na tabela

--- app/crud/features/history_bq.py
+++ app/crud/features/history_bq.py
@@ -12,7 +12,14 @@ class BigQueryStorage:
         self.client = bigquery.Client()
 
     async def store_response(
-        self, dataset_id: str, table_id: str, session_id: str, user_input: str, response: str, response_time: float
+        self,
+        dataset_id: str,
+        table_id: str,
+        session_id: str,
+        user_input: str,
+        response: str,
+        response_time: float,
+        message_type: str = 'text',
     ) -> Result:
         """
         Salva a resposta do agente no BigQuery.
@@ -24,6 +31,7 @@ class BigQueryStorage:
             user_input (str): Texto de entrada fornecido pelo usuário.
             response (str): Resposta gerada pelo agente.
             response_time (float): Tempo de resposta medido.
+            message_type (str): Tipo da mensagem enviada pelo usuário (texto, imagem, audio, location).
 
         Returns:
             Result: Objeto Result contendo sucesso ou erro.
@@ -41,6 +49,7 @@ class BigQueryStorage:
                     'timestamp': datetime.utcnow().isoformat(),
                     'response': response,
                     'response_time': response_time,
+                    'message_type': message_type,
                 }
             ]
 
